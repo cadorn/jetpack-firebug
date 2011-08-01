aebd74e4ea6180973d8d253c005deedec30214e1
cadorn <christoph@christophdorn.com>
2011-08-01 14:59:12 -0700

    formatting fixes

--- examples/HelloWorld/lib/main.js
+++ examples/HelloWorld/lib/main.js
@@ -4,41 +4,41 @@ var FIREBUG = require("jetpack-firebug/firebug");
 
 FIREBUG.registerModuleFactory(function(Window, Firebug, FBL, FBTrace)
 {
-	Firebug.HelloWorldModule = FBL.extend(Firebug.Module,
-	{
-	    dispatchName: "HelloWorldModule",
+    Firebug.HelloWorldModule = FBL.extend(Firebug.Module,
+    {
+        dispatchName: "HelloWorldModule",
 
-	    initialize: function()
-	    {
-	        Firebug.Module.initialize.apply(this, arguments);
+        initialize: function()
+        {
+            Firebug.Module.initialize.apply(this, arguments);
 
-	        FBTrace.sysout("helloWorld.HelloWorldModule.initialize;");
-	    }
-	});
-	return Firebug.HelloWorldModule;
+            FBTrace.sysout("helloWorld.HelloWorldModule.initialize;");
+        }
+    });
+    return Firebug.HelloWorldModule;
 });
 
 FIREBUG.registerPanelFactory(function(Window, Firebug, FBL, FBTrace)
 {
-	Firebug.HelloWorldPanel = function HelloWorldPanel() {}
-	Firebug.HelloWorldPanel.prototype = FBL.extend(Firebug.Panel,
-	{
-	    name: "helloWord",
-	    title: "HelloWorld",
-	    searchable: true,
-	    dispatchName: "HelloWorldPanel",
+    Firebug.HelloWorldPanel = function HelloWorldPanel() {}
+    Firebug.HelloWorldPanel.prototype = FBL.extend(Firebug.Panel,
+    {
+        name: "helloWord",
+        title: "HelloWorld",
+        searchable: true,
+        dispatchName: "HelloWorldPanel",
 
-	    initialize: function(context, document)
-	    {
-	        Firebug.Panel.initialize.apply(this, arguments);
+        initialize: function(context, document)
+        {
+            Firebug.Panel.initialize.apply(this, arguments);
 
-	        FBTrace.sysout("helloWorld.HelloWorldPanel.initialize;");
-	    },
+            FBTrace.sysout("helloWorld.HelloWorldPanel.initialize;");
+        },
 
-	    show: function(state)
-	    {
-	        this.panelNode.innerHTML = "<h2>Hello World!</h2>";
-	    }
-	});	
-	return Firebug.HelloWorldPanel;
+        show: function(state)
+        {
+            this.panelNode.innerHTML = "<h2>Hello World!</h2>";
+        }
+    }); 
+    return Firebug.HelloWorldPanel;
 });
--- examples/Reloading/lib/main.js
+++ examples/Reloading/lib/main.js
@@ -1,65 +1,65 @@
 
 var FIREBUG = require("jetpack-firebug/firebug"),
-	TIMERS = require("timers");
+    TIMERS = require("timers");
 
 
 var reloadCounter = 0;
 
 FIREBUG.registerModuleFactory(function(Window, Firebug, FBL, FBTrace)
 {
-	Firebug.ReloadingModule = FBL.extend(Firebug.Module,
-	{
-	    dispatchName: "ReloadingModule",
+    Firebug.ReloadingModule = FBL.extend(Firebug.Module,
+    {
+        dispatchName: "ReloadingModule",
 
-	    initialize: function()
-	    {
-	        Firebug.Module.initialize.apply(this, arguments);
+        initialize: function()
+        {
+            Firebug.Module.initialize.apply(this, arguments);
 
-	        FBTrace.sysout("reloading.ReloadingModule.initialize;");
-	    }
-	});
-	return Firebug.ReloadingModule;
+            FBTrace.sysout("reloading.ReloadingModule.initialize;");
+        }
+    });
+    return Firebug.ReloadingModule;
 });
 
 FIREBUG.registerPanelFactory(function(Window, Firebug, FBL, FBTrace)
 {
-	Firebug.ReloadingPanel = function ReloadingPanel() {}
-	Firebug.ReloadingPanel.prototype = FBL.extend(Firebug.Panel,
-	{
-	    name: "reloading",
-	    title: "Reloading",
-	    searchable: true,
-	    dispatchName: "ReloadingPanel",
+    Firebug.ReloadingPanel = function ReloadingPanel() {}
+    Firebug.ReloadingPanel.prototype = FBL.extend(Firebug.Panel,
+    {
+        name: "reloading",
+        title: "Reloading",
+        searchable: true,
+        dispatchName: "ReloadingPanel",
 
-	    initialize: function(context, document)
-	    {
-	        Firebug.Panel.initialize.apply(this, arguments);
+        initialize: function(context, document)
+        {
+            Firebug.Panel.initialize.apply(this, arguments);
 
-	        FBTrace.sysout("reloading.ReloadingPanel.initialize;");
-	    },
+            FBTrace.sysout("reloading.ReloadingPanel.initialize;");
+        },
 
-	    show: function(state)
-	    {
-	        this.panelNode.innerHTML = [
-	            '<h2>Reloading Hello World! Counter: ' + reloadCounter + '</h2>',
-  	            '<p><a class="reload-link" href="#">Reload!</a></p>'
-	        ].join("\n");
+        show: function(state)
+        {
+            this.panelNode.innerHTML = [
+                '<h2>Reloading Hello World! Counter: ' + reloadCounter + '</h2>',
+                '<p><a class="reload-link" href="#">Reload!</a></p>'
+            ].join("\n");
 
-	        this.panelNode.getElementsByClassName("reload-link").item(0).addEventListener("click", function()
-	        {
-	        	reloadCounter++;
+            this.panelNode.getElementsByClassName("reload-link").item(0).addEventListener("click", function()
+            {
+                reloadCounter++;
 
-				FIREBUG.internal.unattach();
+                FIREBUG.internal.unattach();
 
-				TIMERS.setTimeout(function()
-				{
-					FIREBUG.internal.attach();
-				}, 2000);
+                TIMERS.setTimeout(function()
+                {
+                    FIREBUG.internal.attach();
+                }, 2000);
 
-	        }, false);
-	    }
-	});	
-	return Firebug.ReloadingPanel;
+            }, false);
+        }
+    }); 
+    return Firebug.ReloadingPanel;
 });
 
 
